87abbb0a9f5727e9b06ad5337253a08a29595978
wejafoo <wes@weja.us>
2021-03-31 08:02:40 -0600

    Add dynamic content components, service, and routing with integration to micro-cms   * Tweak linting configuration   * Remove unused Google Tag Manager pushTag callback   * Update deprecated 'async' to use 'waitForAsync' method for relevant test components

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,26 +1,19 @@
 
 
-import {	async,
-			ComponentFixture,
-			TestBed				} from '@angular/core/testing';
-import {	HomeComponent		} from './home.component';
-import {}	from 'jasmine';
-
+import { waitForAsync		} from '@angular/core/testing';
+import { ComponentFixture	} from '@angular/core/testing';
+import { TestBed			} from '@angular/core/testing';
+import { HomeComponent		} from './home.component';
 
 describe('HomeComponent', () => {
-	
 	let component:	HomeComponent;
 	let fixture:	ComponentFixture<HomeComponent>;
 	
-	beforeEach( async(() => TestBed.configureTestingModule({ declarations: [ HomeComponent ]}).compileComponents()));
-	
+	beforeEach( waitForAsync(() => TestBed.configureTestingModule({ declarations: [HomeComponent]}).compileComponents()));
 	beforeEach(() => {
-		
 		fixture		= TestBed.createComponent( HomeComponent );
 		component	= fixture.componentInstance;
-		
 		fixture.detectChanges()
 	});
-	
 	it('should create', () => expect( component ).toBeTruthy())
 })
--- src/app/nav/nav.component.spec.ts
+++ src/app/nav/nav.component.spec.ts
@@ -1,24 +1,19 @@
 
 
-import { async				} from '@angular/core/testing';
+import { waitForAsync		} from '@angular/core/testing';
 import { ComponentFixture	} from '@angular/core/testing';
 import { TestBed			} from '@angular/core/testing';
 import { NavComponent		} from './nav.component';
 
 describe('PublicHeadComponent', () => {
-	
 	let component:	NavComponent;
 	let fixture:	ComponentFixture<NavComponent>;
 	
-	beforeEach( async(() => { TestBed.configureTestingModule({ declarations: [NavComponent]}).compileComponents()}));
-	
+	beforeEach( waitForAsync(() => { TestBed.configureTestingModule({ declarations: [NavComponent]}).compileComponents()}));
 	beforeEach(() => {
-		
 		fixture		= TestBed.createComponent(NavComponent);
 		component	= fixture.componentInstance;
-		
 		fixture.detectChanges()
 	});
-	
 	it('should create', () => { expect( component ).toBeTruthy()})
 })
